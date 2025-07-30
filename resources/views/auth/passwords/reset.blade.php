dcb2495fbf9005fcaf3a1df3f0d8ec21afaccf57
tamaki404 <nicole.tumpag2003@gmail.com>
2025-07-30 09:36:17 +0800

    Reset view UI and valication errors added

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,8 +1,49 @@
-<form method="POST" action="{{ route('password.update') }}">
-    @csrf
-    <input type="hidden" name="token" value="{{ $token }}">
-    <input type="email" name="email" required placeholder="Your email">
-    <input type="password" name="password" required placeholder="New password">
-    <input type="password" name="password_confirmation" required placeholder="Confirm password">
-    <button type="submit">Reset Password</button>
-</form>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/emailReset.css') }}">
+
+    <title>Reset password</title>
+</head>
+<body>
+
+
+    <div class="resetFrame">
+
+        <img src="{{ asset(path: 'assets/sunnyLogo1.png') }}" alt="Logo" class="logo">
+        <h2>Password Reset</h2>
+        <span><p>{{ old('email', request('email')) }}</p>you can now reset your password</span>
+       
+        <form method="POST" action="{{ route('password.update') }}">
+            @csrf
+            <div class="emailInput">
+                <p>New password</p>
+                <input type="password" name="password" required placeholder=" ">
+            </div>
+            <div class="emailInput" style="margin-top: 7px">
+                <p>Confirm password</p>
+                <input type="password" name="password_confirmation" required placeholder="">
+            </div>
+            
+            <button type="submit">Reset Password</button>
+        </form>
+
+
+        @if ($errors->has('password'))
+            <p class="error-password">{{ $errors->first('password') }}</p>
+        @endif
+        @if ($errors->has('password_confirmation'))
+            <p class="error-password">{{ $errors->first('password_confirmation') }}</p>
+        @endif
+
+
+
+        <a href="{{ route('login') }}">Back to Login</a>
+    </div>
+
+</body>
+</html>
